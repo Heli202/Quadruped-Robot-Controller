cb186aa6da5ff63fb282ee87a1cd9b4241ddd902
Heli202 <158405650+Heli202@users.noreply.github.com>
2024-10-18 13:51:10 +0800

    Fixed up sending and receiving through serial. Just need micropython receiving from serial and sending to servos.

--- Quadruped_Project_VSCode.py
+++ Quadruped_Project_VSCode.py
@@ -27,7 +27,7 @@ class Servo:
 
     def set_value(self, value):
         """Set the value of the servo."""
-        self.value.set(value)
+        self.scale.set(value)
 
 class Leg:
     """Represents a leg of the quadruped, composed of two servos."""
@@ -72,7 +72,7 @@ class Quadruped:
             leg = Leg(parent, leg_id, base_x, base_y)
             self.legs.append(leg)
 
-    def get_all_positions(self):
+    def get_all_positions(self): # These two will have to use statemanager receive command, maybe send a command like "what is the values" and it sends that back?
         """Get positions of all servos in the quadruped."""
 
         pass
@@ -84,17 +84,17 @@ class Quadruped:
 
 class StateManager:
     """Manages saving and loading of robot states."""
-    def __init__(self):
+    def __init__(self, quadruped):
         """Initialize the state manager."""
         self.filename = "states.json"
         self.states_dict = {}
+        self.quadruped = quadruped
 
     def load_states(self):
         """Load states from a file."""
         try:
             with open(self.filename, "r") as file:
                 self.states_dict = json.load(file)
-                print("Loaded states:", self.states_dict)
         except FileNotFoundError:
             print("States file not found, creating file with empty states...")
             self.save_states({}) # Save with empty dictionary if no file found
@@ -107,7 +107,6 @@ class StateManager:
         try:
             with open(self.filename, "w") as outfile:
                 json.dump(self.states_dict, outfile)
-                print("Saved states:", self.states_dict)
         except TypeError:
             print("There was an issue with serialising the dictionary to json format")
         
@@ -117,26 +116,36 @@ class StateManager:
             return self.states_dict[name]
         return
 
-    def set_state(self, name: str, state: str):
+    def set_state(self, state: str):
         """Set a specific state by name."""
-        pass
+        if state is None:
+                print("State does not exist")
+                return
+        for leg in self.quadruped.legs:
+            for servo in leg.servos:
+                if servo.name in state:
+                    servo.set_value(state[servo.name])
+                else:
+                    print(f"No value found for {servo.name} in the loaded state")
 
 class SerialCommunicator:
     """Handles serial communication with the Pico."""
     
-    def __init__(self, port='COM6', baud_rate=115200):
+    def __init__(self, port="COM6", baud_rate=115200):
         """Initialize the serial connection."""
         try:
             self.s = serial.Serial(port, baud_rate)
         except serial.SerialException:
             print("Couldn't find COM port")
             
-    def send_command(self, command: str): # Need to figure out how to send specific / logic
+    def send_command(self, value_list): # Need to figure out how to send specific / logic
         """Send a command to the Pico."""
-        # self.s.write() # Will have to happen when the load state happens with the information from the json file, 
-        # but also how would u know where they are when saving state??? perhaps it needs to happen when loading and when changing slider or just
-        # when slider changes, but have the load change the sliders which in turn sends the command? *** Figure this out
-        pass
+        try:
+            command = bytes(f"{value_list}", encoding= "utf-8") 
+            self.s.write(command)
+            print(f"Command sent: {command.strip()}")
+        except AttributeError:
+            print("The COM port was already occupied")
 
     def receive_data(self):
         """Receive data from the Pico."""
@@ -154,8 +163,9 @@ class QuadrupedGUI:
         self.parent = parent
         self.parent.geometry("1200x600")
         self.parent.title("Quadruped GUI")
-        self.state_manager = StateManager()
         self.quadruped = Quadruped(root)
+        self.state_manager = StateManager(self.quadruped)
+        self.serial_communicator = SerialCommunicator()
         self.create_gui()
 
     def create_gui(self):
@@ -172,7 +182,13 @@ class QuadrupedGUI:
 
     def update_pico(self):
         """Send updated positions to the Pico.""" # Will need to use serial communicator
-        pass
+        value_list = []
+        for leg in self.quadruped.legs:
+            for servo in leg.servos:
+                value = servo.get_value()
+                value_list.append(value)
+        self.serial_communicator.send_command(value_list)
+                
 
     def save_state(self):
         """Save the current state of the robot."""
@@ -184,7 +200,8 @@ class QuadrupedGUI:
         for leg in self.quadruped.legs:
             for servo in leg.servos:
                 states[servo.name] = servo.get_value()
-        self.state_manager.save_states({self.save_state_name: states})
+        self.state_manager.states_dict[self.save_state_name] = states
+        self.state_manager.save_states(self.state_manager.states_dict)
         
 
     def load_state(self):
@@ -196,16 +213,8 @@ class QuadrupedGUI:
             return
         self.state_manager.load_states()
         try:
-            state_data = self.state_manager.get_state(load_state_name)
-            if state_data is None:
-                messagebox.showerror("Error", "State name does not exist")
-                return
-            for leg in self.quadruped.legs:
-                for servo in leg.servos:
-                    if servo.name in state_data:
-                        servo.set_value(state_data[servo.name])
-                    else:
-                        messagebox.showwarning("Warning", f"No value found for {servo.name} in the loaded state")
+            state = self.state_manager.get_state(load_state_name)
+            self.state_manager.set_state(state)
         except Exception:
             messagebox.showerror("Error", f"An error occured while loading the state: {load_state_name}")
     
